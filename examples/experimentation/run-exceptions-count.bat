1943105633653be7558bdafa73ab64b221576f9f
Stefano <stefano.tedeschi@live.it>
2022-02-17 10:39:18 +0100

    Added results timing

--- examples/experimentation/run-exceptions-count.bat
+++ examples/experimentation/run-exceptions-count.bat
@@ -1,11 +1,11 @@
 @echo off
-cd house-building-exceptions-ours
+cd house-building-exceptions-ours-count
 FOR /L %%v IN (1,1,1000) DO (
     echo -------------Iteration %%v OURS-------------
     call gradlew run
 )
 cd ..
-cd house-building-exceptions-message-passing
+cd house-building-exceptions-message-passing-count
 FOR /L %%v IN (1,1,1000) DO (
     echo -------------Iteration %%v MESSAGE PASSING-------------
     call gradlew run
